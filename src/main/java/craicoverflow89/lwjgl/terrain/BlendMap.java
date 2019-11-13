805461847b25218e5fc05f06547b032b589c27ec
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-13 08:59:25 +0000

    Fixed bug where hardcoded file was being used in BlendMap constructor.

--- src/main/java/craicoverflow89/lwjgl/terrain/BlendMap.java
+++ src/main/java/craicoverflow89/lwjgl/terrain/BlendMap.java
@@ -8,7 +8,7 @@ public final class BlendMap {
     private final TerrainTexture texture;
 
     public BlendMap(ModelLoader loader, String file) {
-        texture = new TerrainTexture(loader.loadTexture("terrain/blend/test"));
+        texture = new TerrainTexture(loader.loadTexture(file));
         // NOTE: still using TerrainTexture, as it's far easier, for now
     }
 
