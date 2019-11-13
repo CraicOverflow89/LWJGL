15efce8f5c62bfbaaace4520a743760557809039
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-13 08:53:12 +0000

    Changed the AbstractTexture.textureID property to final (but could also be public, so getTextureID isn't needed).

--- src/main/java/craicoverflow89/lwjgl/textures/AbstractTexture.java
+++ src/main/java/craicoverflow89/lwjgl/textures/AbstractTexture.java
@@ -2,7 +2,7 @@ package craicoverflow89.lwjgl.textures;
 
 public abstract class AbstractTexture {
 
-    private int textureID;
+    private final int textureID;
 
     public AbstractTexture(int textureID) {
         this.textureID = textureID;
